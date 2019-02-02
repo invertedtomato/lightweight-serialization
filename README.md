b80f9652316ea527c7da5e49bb45e416f5cbcde8
Ben Thompson <ben@thompsondigital.com.au>
2019-02-03 07:14:52 +1000

    Updated example for clarity.

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ possible output (even at the expense of functionality). The current design outpe
 ## How do I make it go?
 ```c#
 static void Main(String[] args) {
+    // Create your object
     var input = new POCO {
         Cake = true,
         Vegetable = false,
@@ -15,31 +16,32 @@ static void Main(String[] args) {
             SubValue = true
         }
     };
-    
-    var output = LightWeight.Serialize(input);
 
-    foreach (var c in output) {
-        Console.WriteLine(c);
-    }
+    // Serialize like you normally would
+    var payload = LightWeight.Serialize(input);
 
-    Console.WriteLine("Done");
-    Console.ReadKey();
-}   
+    // Use the outputted byte array
+    Console.WriteLine(BitConverter.ToString(payload));
+
+    // Deserialize like you normally would
+    var output = LightWeight.Deserialize<POCO>(payload);
+}
 
 public class POCO {
-    [LightWeightProperty(0)] 
+    [LightWeightProperty(0)] // Every property to be serialized is decorated with this attribute
     public Boolean Cake;
 
-    public Boolean Ignored;
+    public Boolean Ignored; // If it has no attribute it will be ignored
 
-    [LightWeightProperty(2)] 
+    [LightWeightProperty(2)] // The index is used to identify the field, not the name
     public SubPOCO Sub;
 
-    [LightWeightProperty(1)] 
+    [LightWeightProperty(1)] // For best performance, start with index 0 and increment, though they don't need to be in order
     public Boolean Vegetable;
 }
 
 public class SubPOCO {
-    [LightWeightProperty(2)] public Boolean SubValue;
+    [LightWeightProperty(2)] // Skipping indexes like this may waste bits, but does work
+    public Boolean SubValue;
 }
 ```
--- Tester/Program.cs
+++ Tester/Program.cs
@@ -2,8 +2,9 @@
 
 namespace InvertedTomato.Serialization.LightWeightSerialization {
 	internal class Program {
-		private static void Main(String[] args) {
-			var b = new POCO {
+		static void Main(String[] args) {
+			// Create your object
+			var input = new POCO {
 				Cake = true,
 				Vegetable = false,
 				Sub = new SubPOCO {
@@ -11,28 +12,32 @@ namespace InvertedTomato.Serialization.LightWeightSerialization {
 				}
 			};
 
-			var ret = LightWeight.Serialize(b);
+			// Serialize like you normally would
+			var payload = LightWeight.Serialize(input);
 
-			foreach (var c in ret) {
-				Console.WriteLine(c);
-			}
+			// Use the outputted byte array
+			Console.WriteLine(BitConverter.ToString(payload));
 
-			Console.WriteLine("Done");
-			Console.ReadKey();
+			// Deserialize like you normally would
+			var output = LightWeight.Deserialize<POCO>(payload);
 		}
-	}
 
-	public class POCO {
-		[LightWeightProperty(0)] public Boolean Cake;
+		public class POCO {
+			[LightWeightProperty(0)] // Every property to be serialized is decorated with this attribute
+			public Boolean Cake;
 
-		public Boolean Ignored;
+			public Boolean Ignored; // If it has no attribute it will be ignored
 
-		[LightWeightProperty(2)] public SubPOCO Sub;
+			[LightWeightProperty(2)] // The index is used to identify the field, not the name
+			public SubPOCO Sub;
 
-		[LightWeightProperty(1)] public Boolean Vegetable;
-	}
+			[LightWeightProperty(1)] // For best performance, start with index 0 and increment, though they don't need to be in order
+			public Boolean Vegetable;
+		}
 
-	public class SubPOCO {
-		[LightWeightProperty(2)] public Boolean SubValue;
+		public class SubPOCO {
+			[LightWeightProperty(2)] // Skipping indexes like this may waste bits, but does work
+			public Boolean SubValue;
+		}
 	}
 }
