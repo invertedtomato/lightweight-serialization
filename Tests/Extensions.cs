694ef82ce3d2b0e881a692cbfcfa99048f188856
Ben Thompson <ben.thompson@lup.events>
2020-09-23 20:02:55 +1000

    Merged related files.

--- Tests/Extensions.cs
+++ Tests/Extensions.cs
@@ -12,5 +12,32 @@ namespace Tests
                 .Select(x => Convert.ToByte(target.Substring(x, 2), 16))
                 .ToArray();
         }
+        public static String ToHexString(this UInt16 target, Int32 minBytes = 0)
+        {
+            var output = target.ToString("X").Replace("-", "");
+            output = new String('0', Math.Max(0, minBytes * 2 - output.Length)) + output;
+            return output;
+        }
+
+        public static String ToHexString(this UInt32 target, Int32 minBytes = 0)
+        {
+            var output = target.ToString("X").Replace("-", "");
+            output = new String('0', Math.Max(0, minBytes * 2 - output.Length)) + output;
+            return output;
+        }
+
+        public static String ToHexString(this UInt64 target, Int32 minBytes = 0)
+        {
+            var output = target.ToString("X").Replace("-", "");
+            output = new String('0', Math.Max(0, minBytes * 2 - output.Length)) + output;
+            return output;
+        }
+
+        public static String ToHexString(this Byte[] target, Int32 minBytes = 0)
+        {
+            var output = BitConverter.ToString(target).Replace("-", "");
+            output = new String('0', Math.Max(0, minBytes * 2 - output.Length)) + output;
+            return output;
+        }
     }
 }
